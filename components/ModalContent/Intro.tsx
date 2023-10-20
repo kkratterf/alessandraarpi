d1dac52f01f0ef340f0df131ace0db4af78c55cb
kkratterf <kkratterf@gmail.com>
2023-10-20 08:40:36 +0200

    Fix &apos;

--- components/ModalContent/Intro.tsx
+++ components/ModalContent/Intro.tsx
@@ -7,9 +7,9 @@ function IntroModal() {
   return (
     <div className="relative z-40 text-xl cursor-default overflow-y-scroll max-h-[50vh] p-6 space-y-5 text-black">
       <p>
-        Ciao, sono <b>Alessandra Arpi</b>, sono una <b>copywriter</b> e una <b>consulente di comunicazione</b> e qui c'è il mio sito, che è fatto
-        come il mio <b>desktop</b> vero verissimo: manciate di spunti, tanti
-        file di testo dove appunto cose, un posto dove iscriversi alla <b>newsletter</b>, il mio <b>portoflio</b> in una cartella ordinata e
+        Ciao, sono <b>Alessandra Arpi</b>, sono una <b>copywriter</b> e una <b>consulente di comunicazione</b> e qui c&apos;è il mio sito, che è
+        fatto come il mio <b>desktop</b> vero verissimo: manciate di spunti,
+        tanti file di testo dove appunto cose, un posto dove iscriversi alla <b>newsletter</b>, il mio <b>portoflio</b> in una cartella ordinata e
         tutto il resto sparso.
       </p>
       <p>
