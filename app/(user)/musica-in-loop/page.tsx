095f01c69308a506cca7c368b2e1a505505e3c23
kkratterf <kkratterf@gmail.com>
2023-10-20 21:21:56 +0200

    Fix build

--- app/(user)/musica-in-loop/page.tsx
+++ app/(user)/musica-in-loop/page.tsx
@@ -1,43 +1,23 @@
-'use client';
-
 // Import core
 // Import third parts
-import { motion } from 'framer-motion';
+import { groq } from 'next-sanity';
+import { client } from '../../../lib/sanity.client';
 // Import customs
-import PageContainer from '../../../components/PageContainer';
-import { MusicList } from '../../../typings';
+import MusicList from './MusicList';
 
-type MusicListModalProps = {
-  musicList: MusicList[];
-};
+export const revalidate = 6000;
 
-function MusicPage({ musicList }: MusicListModalProps) {
+const query_musicList = groq`
+  *[_type=='musicList' && visible == true ] {
+    ...,
+  } | order(_createdAt asc)
+`;
+
+export default async function MusicListPage() {
+  const musicList = await client.fetch(query_musicList);
   return (
-    <motion.section
-      initial={{ y: 40, opacity: 0 }}
-      animate={{ y: 0, opacity: 1 }}
-      transition={{ duration: 1.2, delay: 0 }}
-      className="relative z-40"
-    >
-      <PageContainer>
-        <h1 className="font-bold text-4xl pb-6">Musica in loop</h1>
-        <div className="relative z-40 text-lg cursor-default space-y-4 text-black">
-          {musicList.map((musicList) => (
-            <div key={musicList._id}>
-              <iframe
-                className="rounded-md"
-                src={musicList.src}
-                width="100%"
-                height="152"
-                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-                loading="lazy"
-              />
-            </div>
-          ))}
-        </div>
-      </PageContainer>
-    </motion.section>
+    <>
+      <MusicList musicList={musicList} />
+    </>
   );
 }
-
-export default MusicPage;
