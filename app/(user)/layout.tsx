f68d2ca00cf743c762732b159fa593fd518f33b2
kkratterf <kkratterf@gmail.com>
2024-02-06 12:14:30 +0100

    cookie banner

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -4,6 +4,7 @@
 import { useEffect, useState } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 // Import customs
 import '@/styles/globals.css';
 import '@/styles/font.css';
@@ -15,7 +16,6 @@ export default function RootLayout({
 }: {
   children: React.ReactNode;
 }) {
-
   const [loading, setLoading] = useState(true);
 
   return (
@@ -35,6 +35,49 @@ export default function RootLayout({
         </AnimatePresence>
         <Analytics />
       </body>
+      <Script src="https://example.com/script.js" />
+      <Script
+        id="iubenda-cs"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            var _iub = _iub || [];
+            _iub.csConfiguration = {
+              "consentOnContinuedBrowsing": false,
+              "ccpaAcknowledgeOnDisplay": true,
+              "whitelabel": false,
+              "lang": "it",
+              "enableCcpa": true,
+              "countryDetection": true,
+              "googleAdditionalConsentMode": true,
+              "isTCFConsentGlobal": false,
+              "perPurposeConsent": true,
+              "enableTcf": true,
+              "cookiePolicyId": 18667510,
+              "siteId": 2054466,
+              "banner": {
+                "acceptButtonDisplay": true,
+                "customizeButtonDisplay": true,
+                "position": "float-top-center",
+                "rejectButtonDisplay": true,
+                "acceptButtonColor": "#0073CE",
+                "acceptButtonCaptionColor": "white",
+                "customizeButtonColor": "#DADADA",
+                "customizeButtonCaptionColor": "#4D4D4D",
+                "rejectButtonColor": "#0073CE",
+                "rejectButtonCaptionColor": "white",
+                "textColor": "black",
+                "backgroundColor": "white"
+              }
+            };
+          `,
+        }}
+      />
+      <Script src="https://cs.iubenda.com/autoblocking/2054466.js" />
+      <Script src="//cdn.iubenda.com/cs/tcf/stub-v2.js" />
+      <Script src="//cdn.iubenda.com/cs/tcf/safe-tcf-v2.js" />
+      <Script src="//cdn.iubenda.com/cs/ccpa/stub.js" />
+      <Script src="//cdn.iubenda.com/cs/iubenda_cs.js" async />
     </html>
   );
 }
